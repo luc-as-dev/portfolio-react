bbf4757515be444fa23e43ddbaad0d76790a2f33
Lucas Karlsson <lucas.karlsson@post.com>
2023-03-26 22:22:33 +0200

    added WelcomeCard

--- client/src/components/WelcomeCard/WelcomeCard.tsx
+++ client/src/components/WelcomeCard/WelcomeCard.tsx
@@ -9,7 +9,14 @@ export default function WelcomeCard({ className }: Props) {
   const classes = `welcome-card${className ? " " + className : ""}`;
   return (
     <ContentCard className={classes}>
-      <h2>Hey, i'm Lucas, a developer with a big passion for code</h2>
+      <h2 className="welcome-card-title">
+        Greetings! I'm Lucas, the dedicated coder you've been looking for.
+      </h2>
+      <p className="welcome-card-text">
+        As a passionate programmer with a knack for pattern recognition, I've
+        gained proficiency in various languages through years of self-learning,
+        and I'm eager to make a mark in the programming world.
+      </p>
     </ContentCard>
   );
 }
