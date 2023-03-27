8166fd34c1c48e0d994cc4ff73abc98bbe5c80c3
Lucas Karlsson <lucas.karlsson@post.com>
2023-03-27 20:44:22 +0200

    updated styling

--- client/src/components/WelcomeCard/WelcomeCard.tsx
+++ client/src/components/WelcomeCard/WelcomeCard.tsx
@@ -1,3 +1,4 @@
+import backgroundImage from "../../assets/hero-shadow.png";
 import ContentCard from "../UI/ContentCard/ContentCard";
 import "./WelcomeCard.scss";
 
@@ -9,14 +10,16 @@ export default function WelcomeCard({ className }: Props) {
   const classes = `welcome-card${className ? " " + className : ""}`;
   return (
     <ContentCard className={classes}>
-      <h2 className="welcome-card-title">
-        Greetings! I'm Lucas, the dedicated coder you've been looking for.
-      </h2>
-      <p className="welcome-card-text">
-        As a passionate programmer with a knack for pattern recognition, I've
-        gained proficiency in various languages through years of self-learning,
-        and I'm eager to make a mark in the programming world.
-      </p>
+      <div className="welcome-card-inner">
+        <h2 className="welcome-card-inner-title">
+          Greetings! I'm Lucas, the dedicated coder you've been looking for.
+        </h2>
+        <p className="welcome-card-inner-text">
+          As a passionate programmer with a knack for pattern recognition, I've
+          gained proficiency in various languages through years of
+          self-learning, and I'm eager to make a mark in the programming world.
+        </p>
+      </div>
     </ContentCard>
   );
 }
