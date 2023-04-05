7fc4e7db061609a95423696cc54bfe8cfac1640d
Lucas Karlsson <lucas.karlsson@post.com>
2023-04-05 11:29:37 +0200

    changed layout and design

--- client/src/sections/AppContent.tsx
+++ client/src/sections/AppContent.tsx
@@ -1,20 +1,18 @@
 import "./AppContent.scss";
-import AppCard from "../components/UI/AppCard/AppCard";
 import AboutSection from "./AboutSection/AboutSection";
 import ContactSection from "./ContactSection/ContactSection";
 import HomeSection from "./HomeSection/HomeSection";
 import ProjectsSection from "./ProjectsSection/ProjectsSection";
-import { useEffect } from "react";
 
 type Props = {};
 
 export default function AppContent({}: Props) {
   return (
-    <AppCard className="app-content">
+    <div className="app-content">
       <HomeSection />
       <AboutSection />
       <ProjectsSection />
       <ContactSection />
-    </AppCard>
+    </div>
   );
 }
